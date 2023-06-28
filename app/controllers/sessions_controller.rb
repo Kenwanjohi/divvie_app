461ec10018c0dbd3acc109fa099e2a6364427e8f
Kenwanjohi <wanjohiken85@gmail.com>
2023-06-28 13:15:05 +0300

    Add login and logout actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user&.authenticate(params[:user][:password])
+      login @user
+      redirect_to root_path, notice: "Signed in successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, notice: "Signed out"
+  end
 end
